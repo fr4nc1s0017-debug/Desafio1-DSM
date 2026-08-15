d2248c868420ecce86d68f28644d2652be73b4eb
cheche <fr4nc1s0017@gmail.com>
2026-08-14 21:49:49 -0600

    Vista de pantalla de promedio y variables de PromedioActivity.kt; logica de direccionamiento de pantalla de SalarioActivity.kt

--- app/src/main/java/com/example/desafio1_menu/PromedioActivity.kt
+++ app/src/main/java/com/example/desafio1_menu/PromedioActivity.kt
@@ -1,4 +1,34 @@
 package com.example.desafio1_menu
 
-class PromedioActivity {
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import kotlin.math.pow
+import kotlin.math.sqrt
+
+class PromedioActivity : AppCompatActivity(){
+
+    private lateinit var Nombre_Estudiante: EditText
+    private lateinit var Nota1: EditText
+    private lateinit var Nota2: EditText
+    private lateinit var Nota3: EditText
+    private lateinit var Nota4: EditText
+    private lateinit var Nota5: EditText
+    private lateinit var btnCalcular: Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.promedio)
+
+        Nombre_Estudiante = findViewById(R.id.etEstudiante)
+        Nota1 = findViewById(R.id.etNota1)
+        Nota2 = findViewById(R.id.etNota2)
+        Nota3 = findViewById(R.id.etNota3)
+        Nota4 = findViewById(R.id.etNota4)
+        Nota5 = findViewById(R.id.etNota5)
+        btnCalcular = findViewById(R.id.btnCalcular)
+    }
 }
--- app/src/main/java/com/example/desafio1_menu/SalarioActivity.kt
+++ app/src/main/java/com/example/desafio1_menu/SalarioActivity.kt
@@ -1,4 +1,20 @@
 package com.example.desafio1_menu
 
-class SalarioActivity {
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import kotlin.math.pow
+import kotlin.math.sqrt
+
+class SalarioActivity  : AppCompatActivity(){
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.salario)
+
+    }
 }
